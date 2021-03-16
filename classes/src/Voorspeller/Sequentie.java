17b9e2a96038be5d49860a7ad89c9b5e78e471c4
jaspe <Jasper_versantvoort@hotmail.com>
2021-03-16 15:09:32 +0100

    Javadoc toegevoegd

--- classes/src/Voorspeller/Sequentie.java
+++ classes/src/Voorspeller/Sequentie.java
@@ -2,6 +2,12 @@ package Voorspeller;
 
 import java.util.ArrayList;
 
+/**
+ * Sequentie class
+ *
+ * @author Margo en Jasper
+ */
+
 public class Sequentie {
 
     private String sequentie;
@@ -20,7 +26,15 @@ public class Sequentie {
 
     }
 
+    /**
+     * Deze functie is gemaakt om ORF's te zoeken in een sequentie.
+     *
+     * @param seq  is de sequentie waar je ORF's van wilt vinden
+     * @param head is de header van de sequentie
+     * @return een ArrayList van de ORF's
+     */
     private ArrayList<ORF> zoekORF(String seq, String head) {
+
 
         ArrayList<ORF> gev_orf = new ArrayList<>();
 
@@ -28,9 +42,8 @@ public class Sequentie {
         ArrayList<Integer> stop_pos = new ArrayList<>();
 
 
-
-        for(int i = 0; i< seq.length(); i++) {
-            if (seq.startsWith("ATG", i)){
+        for (int i = 0; i < seq.length(); i++) {
+            if (seq.startsWith("ATG", i)) {
                 start_pos.add(i);
             }
             if (seq.startsWith("TAA", i) || seq.startsWith("TAG", i) || seq.startsWith("TGA", i)) {
@@ -38,16 +51,17 @@ public class Sequentie {
             }
 
         }
-        for (int i: start_pos) {
+        for (int i : start_pos) {
             for (int x : stop_pos) {
-                if (i%3 == x%3 && i<x) {
-                    String orf_Seq = seq.substring(i, x+3);
-                    int rf = i%3+1;
-                    gev_orf.add(new ORF(orf_Seq,rf,i,head));
+                if (i % 3 == x % 3 && i < x) {
+                    String orf_Seq = seq.substring(i, x + 3);
+                    int rf = i % 3 + 1;
+                    gev_orf.add(new ORF(orf_Seq, rf, i, head));
 
                 }
             }
-        } return gev_orf;
+        }
+        return gev_orf;
 
 
     }
