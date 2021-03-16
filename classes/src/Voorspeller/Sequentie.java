e720172054c558f17ae2abc83a4090a7a4117f48
jaspe <Jasper_versantvoort@hotmail.com>
2021-03-16 15:02:29 +0100

    zoekorf functie gemaakt Margo en Jasper

--- classes/src/Voorspeller/Sequentie.java
+++ classes/src/Voorspeller/Sequentie.java
@@ -1,5 +1,7 @@
 package Voorspeller;
 
+import java.util.ArrayList;
+
 public class Sequentie {
 
     private String sequentie;
@@ -10,10 +12,44 @@ public class Sequentie {
 
     public Sequentie(String sequentie, String header) {
 
-        setSequentie(sequentie);
+        setSequentie(sequentie.toUpperCase());
         setHeader(header);
         setLengte();
         setCheckDNA();
+        System.out.println(zoekORF(sequentie.toUpperCase(), header));
+
+    }
+
+    private ArrayList<ORF> zoekORF(String seq, String head) {
+
+        ArrayList<ORF> gev_orf = new ArrayList<>();
+
+        ArrayList<Integer> start_pos = new ArrayList<>();
+        ArrayList<Integer> stop_pos = new ArrayList<>();
+
+
+
+        for(int i = 0; i< seq.length(); i++) {
+            if (seq.startsWith("ATG", i)){
+                start_pos.add(i);
+            }
+            if (seq.startsWith("TAA", i) || seq.startsWith("TAG", i) || seq.startsWith("TGA", i)) {
+                stop_pos.add(i);
+            }
+
+        }
+        for (int i: start_pos) {
+            for (int x : stop_pos) {
+                if (i%3 == x%3 && i<x) {
+                    String orf_Seq = seq.substring(i, x+3);
+                    int rf = i%3+1;
+                    gev_orf.add(new ORF(orf_Seq,rf,i,head));
+
+                }
+            }
+        } return gev_orf;
+
+
     }
 
     public String getSequentie() {
